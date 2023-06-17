7e1662a8125f77a8694f90300d99a4ed70dd6556
jellylovs <auliasals2002@gmail.com>
2023-06-17 10:33:27 +0700

    done

---
+++ app copy.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pickle
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+""" bingung gimana cara pakai modelnya
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=10000, output_dim=40),
+    tf.keras.layers.LSTM(64),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+"""
+
+model_LSTM = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=10000, output_dim=40),
+    tf.keras.layers.LSTM(64),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+@st.cache_resource
+def load_models():
+    """
+    @st.cache_resource decorator digunakan untuk menyimpan resource model.
+
+    Fungsi load_models() akan membuat model FCDUG dan menerapkan weights dari file .h5 
+
+    """
+    model_LSTM.load_weights("LSTM.h5")
+
+    return model
+
+model = load_models()
+
+
+
+
+def main():
+    st.title("Text Classification Depression")
+    st.subheader("Hello Streamlit")
+
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        with st.form(key='nlpForm'):
+            raw_text = st.text_area("Enter Text Here")
+            submit_button = st.form_submit_button(label='Analyze')
+            
+            #melakukan tokenisasi
+            tokenizer = Tokenizer(num_words=10000, oov_token='<UNK>')
+            #tokenizer.fit_on_texts(submit_button) 
+            sequences = tokenizer.texts_to_sequences(raw_text)
+            padded = pad_sequences(sequences, maxlen=200, padding='post', truncating='post')
+            p = np.argmax(model.predict(padded))
+
+            #melakukan padding sequence
+            # def get_sequences(tokenizer, tweets):
+            #     sequences = tokenizer.texts_to_sequences(tweets)
+            #     padded_sequences = pad_sequences(sequences, truncating='post', maxlen=40, padding='post')
+            #     return padded_sequences 
+
+            # train_sequences = get_sequences(tokenizer, submit_button)
+
+
+            #model
+
+            #prediction
+            @st.cache_data()
+            def predict(text_predict):
+                """
+                @st.cache_data decorator berfungsi untuk caching / menyimpan data prediksi sementara
+
+                Fungsi predict digunakan untuk melakukan prediksi data
+                """
+                image = preprocess_image(image_predict)
+                prediction = model.predict(image)
+                
+                return prediction
+
+        col1,col2 = st.columns(2)
+        if submit_button:
+            with col1:
+                st.info("Results")
+            with col2:
+                st.info("Word Classification")
+    
+    else:
+        st.subheader("About")
+
+if __name__=='__main__':
+    main()
--- app.py
+++ app.py
@@ -1,29 +1,10 @@
 import streamlit as st
-import pickle
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-""" bingung gimana cara pakai modelnya
-model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(input_dim=10000, output_dim=40),
-    tf.keras.layers.LSTM(64),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(2, activation='softmax')
-])
-"""
 
-model_LSTM = tf.keras.Sequential([
-    tf.keras.layers.Embedding(input_dim=10000, output_dim=40),
-    tf.keras.layers.LSTM(64),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(2, activation='softmax')
-])
 
 @st.cache_resource
 def load_models():
@@ -33,14 +14,57 @@ def load_models():
     Fungsi load_models() akan membuat model FCDUG dan menerapkan weights dari file .h5 
 
     """
+
+    model_LSTM = tf.keras.Sequential([
+    tf.keras.layers.Embedding(input_dim=10000, output_dim=40),
+    tf.keras.layers.LSTM(64),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+    ])
+    
+
     model_LSTM.load_weights("LSTM.h5")
 
-    return model
+    return model_LSTM
 
 model = load_models()
 
 
+def preprocess_text(sentence):
+    sentence = [sentence]
+    print(sentence)
 
+    # Preprocessing
+    # melakukan tokenisasi
+    tokenizer = Tokenizer(num_words=10000, oov_token='<UNK>')
+    tokenizer.fit_on_texts(sentence)
+    #tokenizer.fit_on_texts(sentence)
+    sequences = tokenizer.texts_to_sequences(sentence)
+    #sequences = tokenizer.fit_on_texts(sentence)
+    print(sequences)
+    padded = pad_sequences(sequences, maxlen=40, padding='post', truncating='post')
+    print("Padded in preprocess ->", padded)
+
+    return padded
+
+def predict(text_predict):
+    """
+    @st.cache_data decorator berfungsi untuk caching / menyimpan data prediksi sementara
+
+    Fungsi predict digunakan untuk melakukan prediksi data
+    """
+    padded = preprocess_text(text_predict)
+    print(padded)
+    prediction = int(np.argmax(model.predict(padded)))
+
+    print("Prediction -> ", prediction)
+    if prediction == 0:
+        prediction = "Tidak Depresi"
+    else:
+        prediction = "Depresi"
+    
+    return prediction
 
 def main():
     st.title("Text Classification Depression")
@@ -54,44 +78,13 @@ def main():
         with st.form(key='nlpForm'):
             raw_text = st.text_area("Enter Text Here")
             submit_button = st.form_submit_button(label='Analyze')
-            
-            #melakukan tokenisasi
-            tokenizer = Tokenizer(num_words=10000, oov_token='<UNK>')
-            #tokenizer.fit_on_texts(submit_button) 
-            sequences = tokenizer.texts_to_sequences(raw_text)
-            padded = pad_sequences(sequences, maxlen=200, padding='post', truncating='post')
-            p = np.argmax(model.predict(padded))
 
-            #melakukan padding sequence
-            # def get_sequences(tokenizer, tweets):
-            #     sequences = tokenizer.texts_to_sequences(tweets)
-            #     padded_sequences = pad_sequences(sequences, truncating='post', maxlen=40, padding='post')
-            #     return padded_sequences 
-
-            # train_sequences = get_sequences(tokenizer, submit_button)
-
-
-            #model
-
-            #prediction
-            @st.cache_data()
-            def predict(text_predict):
-                """
-                @st.cache_data decorator berfungsi untuk caching / menyimpan data prediksi sementara
-
-                Fungsi predict digunakan untuk melakukan prediksi data
-                """
-                image = preprocess_image(image_predict)
-                prediction = model.predict(image)
+            col1  = st.columns(1)
+            if submit_button:
                 
-                return prediction
-
-        col1,col2 = st.columns(2)
-        if submit_button:
-            with col1:
+                result = predict(raw_text)
                 st.info("Results")
-            with col2:
-                st.info("Word Classification")
+                st.write(result)
     
     else:
         st.subheader("About")
